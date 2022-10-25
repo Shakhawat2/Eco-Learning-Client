c291cf2defc21d47becb5825abbf037589570636
Shakhawat2 <shohugmahbub@gmail.com>
2022-10-25 23:08:44 +0600

    added all course

--- src/Pages/LeftCourses.js
+++ src/Pages/LeftCourses.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from '../Contexts/UserContext';
 
 const LeftCourses = () => {
+    const {user} = useContext(AuthContext)
     return (
         <div className=" h-40 lg:h-screen p-3  w-screen  lg:w-60 dark:bg-black dark:text-gray-100">
             <div className="flex items-center p-2 space-x-4">
                 <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="w-12 h-12 rounded-full dark:bg-gray-500" />
                 <div>
-                    <h2 className="text-lg font-semibold">Leroy Jenkins</h2>
+                    <h2 className="text-lg font-semibold">{user?.displayName}</h2>
                     <span className="flex items-center space-x-1">
                         <a rel="noopener noreferrer" href="#" className="text-xs hover:underline dark:text-gray-400">View profile</a>
                     </span>
